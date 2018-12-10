79b578d82afe1eb600912bbfba9ca7451b5db6c4
jbm <jbmeslin@onewealthplace.com>
2018-12-11 00:22:06 +0100

    add nats pusblish

--- example/sse-client.js
+++ example/sse-client.js
@@ -1,5 +1,30 @@
 var EventSource = require('..')
 var es = new EventSource('http://localhost:1234/sse')
+
+
+var NATS = require('nats');
+
+var servers = ['nats://localhost:4222'];
+
+// Randomly connect to a server in the cluster group.
+// var nats = nats.connect({'servers': servers});
+
+// currentServer is the URL of the connected server.
+var nats = NATS.connect();
+console.log("Connected to " + nats.currentServer.url.host);
+
+// Simple Publisher
+
+
+// Simple Subscriber
+nats.subscribe('foo', function(msg) {
+  console.log('Received a message: ' + msg);
+});
+
 es.addEventListener('server-time', function (e) {
+
+
+  nats.publish('foo', e.data);
+
   console.log(e.data)
 })
